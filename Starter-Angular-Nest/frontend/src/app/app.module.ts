fd087bef83f99409e3d1f78ceb07af6cb71e7867
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-06-27 20:38:55 +0200

    wip front

--- Starter-Angular-Nest/frontend/src/app/app.module.ts
+++ Starter-Angular-Nest/frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TaskFormComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
